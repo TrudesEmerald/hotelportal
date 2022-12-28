49c95da683851087d1bb9ae7fc7cf48327d579b7
TrudesEmerald <getrudekyeikbh@gmail.com>
2022-12-28 13:01:07 +0000

    new5

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
 
-function myFunction(){document.getElementById("chcolor").style.backgroundColor="red"; }
+/*function myFunction(){document.getElementById("chcolor").style.backgroundColor="red"; } */
 
+const a=document.getElementById("chcolor");
+
+if (localStorage.getItem("chcolorClicked")){
+    a.style.background="green";
+}
+else{
+    a.addEventListener("click",(e) => {
+        e.preventDefault();
+        a.style.background="green";
+        localStorage.setItem("chcolorClicked", true)
+    });
+}
 
 
 
